7a48430acec5d8dde65ad67f2846450d61ba466e
bruno-hsl <bruno.hsl.077@gmail.com>
2024-11-15 20:35:51 -0300

    footer

--- mtcell/src/components/Footer.jsx
+++ mtcell/src/components/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import '../styles/Footer.css'
+import '../styles/Footer.css';
+import instagramIcon from '../img/instagram.png';
+import whatsappIcon from '../img/whatsapp.png';
 
 function Footer() {
   return (
-    <footer>&copy; Desenvolvido por estudantes da Universidade Católica de Brasília</footer>
+    <footer className="footer">
+      <div className="redesSociais">
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <img src={instagramIcon} alt="Instagram"  />
+        </a>
+        <a href="htt  ps://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <img src={whatsappIcon} alt="whatsapp"  />
+        </a>
+      </div>
+      <p>&copy; Desenvolvido por estudantes da Universidade Católica de Brasília</p>
+    </footer>
   );
 }
 
