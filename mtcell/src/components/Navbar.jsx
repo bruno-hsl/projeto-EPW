a6d0e229642813f5568eb6b4b15558a5999ef136
InfratorXD <guiguicazeee@gmail.com>
2024-11-22 16:50:50 -0300

    mais atualizações

--- mtcell/src/components/Navbar.jsx
+++ mtcell/src/components/Navbar.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import WhatsAppNavbar from '../img/WhatsAppNavbar.png';
+import CelularDropdown from '../img/CelularDropdown.png';
+import TabletDropdown from '../img/TabletDropdown.png';
+import RelogioDropdown from '../img/RelogioDropdown.png';
+import CaixaSomDropdown from '../img/CaixaSomDropdown.png';
 
 
 function Navbar() {
@@ -42,15 +47,28 @@ function Navbar() {
         <li class="dropdown">
           <a href="#">Produtos</a>
           <ul class="dropdown-menu">
-            <li><Link to="/celulares">Celulares</Link></li>
-            <li><a href="/tablets">Tablets</a></li>
-            <li><a href="/relogios">Relógios</a></li>
-            <li><a href="/produtos/videogames">Video games</a></li>
-            <li><a href="/produtos/caixa_de_som">Caixa de som</a></li>
+          <li><Link to="/celulares">
+            <img src={CelularDropdown} alt="Celulares" class="dropdown-item-image"/>
+            Celulares</Link></li>
+            <li><Link to="/tablets">
+            <img src={TabletDropdown} alt="Tablets" class="dropdown-item-image"/>
+            Tablets</Link></li>
+            <li><Link to="/relogios">
+            <img src={RelogioDropdown} alt="Relogios" class="dropdown-item-image"/>
+            Relógios</Link></li>
+            <li><Link to="/produtos/caixa_de_som">
+            <img src={CaixaSomDropdown} alt="Relogios" class="dropdown-item-image"/>
+            Caixa de som</Link></li>
           </ul>
         </li>
         <li><Link to="/sobre-nos">Sobre Nós</Link></li>
       </ul>
+      <li class="Navbar-contato">
+        <a href="https://wa.me/556185490894" target="_blank" rel="noopener noreferrer">
+          <img src={WhatsAppNavbar} alt="whatsapp" class="Navbar-contato-icon" />
+          <span>Contato</span>
+        </a>
+      </li>
     </nav>
   );
 }
